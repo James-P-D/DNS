956b7018b0fb2bdab5e89a185d0c1d33e2134868
James Dorrington <jdorrington@hotmail.com>
2019-11-23 17:42:30 +0000

    Sending now working. Needed to replace periods in lookup_name with 0x02

--- src/DNS.rb
+++ src/DNS.rb
@@ -21,11 +21,9 @@ def usage()
 end
 
 def create_packet(lookup_host, transaction_ID)
-  #packet = Array.new
   packet = [transaction_ID, 0x0100, 0x0001, 0x0000, 0x0000, 0x0000, 0x03].pack("S>S>S>S>S>S>C")
-  packet += lookup_host
+  packet += lookup_host.gsub('.', 0x02.chr)
   packet += [0x00, 0x0001, 0x0001].pack("CS>S>")
-
   return packet
 end
 
@@ -53,7 +51,7 @@ def main()
   sender.send(packet, 0, domain_host, DOMAIN_PORT)
   sender_port = sender.addr[1]
   puts "sent over port %d" % sender_port
-  # Tell the user the total of open/closed ports
+  
   puts "Complete."
 
   # Exit to OS
