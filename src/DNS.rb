08b1465af7214c229faa076f4aa317e1c6be6d91
James Dorrington <jdorrington@hotmail.com>
2019-11-23 17:28:08 +0000

    Sending of UDP packets is working, but they're still malformed.

--- src/DNS.rb
+++ src/DNS.rb
@@ -8,6 +8,7 @@ require 'socket'
 # Global Variables                                               #
 ##################################################################
 
+DOMAIN_PORT = 53
 
 ##################################################################
 # usage() - Tells user what parameters are available             #
@@ -17,6 +18,15 @@ def usage()
   puts "ruby DNS.rb domain-host lookup-host"
   puts "e.g. ruby DNS.rb 192.168.0.1 news.bbc.co.uk"
   exit(0)
+end
+
+def create_packet(lookup_host, transaction_ID)
+  #packet = Array.new
+  packet = [transaction_ID, 0x0100, 0x0001, 0x0000, 0x0000, 0x0000, 0x03].pack("S>S>S>S>S>S>C")
+  packet += lookup_host
+  packet += [0x00, 0x0001, 0x0001].pack("CS>S>")
+
+  return packet
 end
 
 ##################################################################
@@ -35,6 +45,14 @@ def main()
   domain_host = ARGV[0]
   lookup_host = ARGV[1]
   
+  trancation_ID = 0
+  packet = create_packet(lookup_host, trancation_ID)
+
+  sender = UDPSocket.new()
+
+  sender.send(packet, 0, domain_host, DOMAIN_PORT)
+  sender_port = sender.addr[1]
+  puts "sent over port %d" % sender_port
   # Tell the user the total of open/closed ports
   puts "Complete."
 
