96c31e32ba005f7053522623ed7ce1a46fa65136
James Dorrington <jdorrington@hotmail.com>
2019-11-27 07:16:06 +0000

    Still parsing answer responses...

--- src/DNS.rb
+++ src/DNS.rb
@@ -17,6 +17,7 @@ ADDITIONAL_RRS = 0x0000
 NULL = 0x00
 TYPE_A = 0x0001
 CLASS_IN = 0x0001
+TYPE_C_NAME = 0x0005
 
 ##################################################################
 # usage() - Tells user what parameters are available             #
@@ -66,27 +67,106 @@ def create_packet(lookup_host, transaction_ID)
 end
 
 ##################################################################
+# decode_host() - Decodes the hostname received from DNS service #
+# Note this is basically the opposite of encode_lookup_host()    #
+# since the data received in the same format whereby each string #
+# is preceeded by the string's length, e.g.                      #
+# {4}new{3}bbc{2}co{2}uk{0}                                      #
+##################################################################
+
+def decode_host(data_received, answer_offset, answer_data_length)
+  host_str = ""
+  
+  i = 0
+  while i <answer_data_length
+    byte_read = data_received.slice(answer_offset + i, data_received.length - (answer_offset + i)).unpack("C")
+    if(byte_read[0] == 0)
+      break
+    end
+
+    i = i + 1
+    for j in 0..(byte_read[0]-1)
+      char_read = data_received.slice(answer_offset + i, data_received.length - (answer_offset + i)).unpack("C")
+      i = i + 1
+      host_str << char_read[0].chr()
+    end
+
+    if i < answer_data_length -1
+      host_str << "."
+    end
+  end
+
+  return host_str
+end
+
+##################################################################
+# decode_ip() - Decodes and IP address.                          #
+##################################################################
+
+def decode_ip(data_received, answer_offset, answer_data_length)
+  host_str = ""
+  
+  i = 0
+  while i < answer_data_length
+    byte_read = data_received.slice(answer_offset + i, data_received.length - (answer_offset + i)).unpack("C")
+
+    host_str << byte_read[0].to_s
+    if i < answer_data_length - 1
+      host_str << "."
+    end
+    i = i + 1
+  end
+
+  return host_str
+end
+
+##################################################################
+# parse_answer_host() - Parses the answer host data (string      #
+# hostname, IP address, etc.)                                    #
+##################################################################
+
+def parse_answer_host(data_received, answer_offset, answer_data_length, answer_type)
+  
+  if(answer_type == TYPE_A)
+    return decode_ip(data_received, answer_offset, answer_data_length)
+  elsif(answer_type == TYPE_C_NAME)    
+    return decode_host(data_received, answer_offset, answer_data_length)
+  end
+
+  return "UNKNOWN"
+end
+
+##################################################################
 # parse_response_packet(data_received) - Parse the received data #
 # into an array of answers                                       #
 ##################################################################
 
 def parse_response_packet(data_received)
   begin
-    answers = Array.new
-    transaction_ID, flags, questions, answers, authority_rrs, additional_rrs, lookup_host = data_received.unpack("S>S>S>S>S>S>Z*")
-    lookup_host = decode_lookup_host_buffer(data_received)
+    answers_received = Array.new
+    transaction_ID, flags, questions, answers, authority_rrs, additional_rrs, lookup_host, query_type, query_class = data_received.unpack("S>S>S>S>S>S>Z*S>S>")
+    
+    offset = (2 * 6) + (lookup_host.length + 1) + (2 * 2)
+    answer_offset = offset
 
+    for answer_index in 1..answers      
+      answer_name, answer_type, answer_class, answer_time_to_live, answer_data_length = data_received.slice(answer_offset, data_received.length - offset).unpack("S>S>S>L>S>")
+      answer_offset += (2 * 3) + (1 * 4) + (2 * 1)
+      answer_host = parse_answer_host(data_received, answer_offset, answer_data_length, answer_type)
+      puts answer_host
+      answer_offset += answer_data_length
+    end
     #answer = OpenStruct.new
     #answer.type = "foo"
     #answer.time_to_live = 123
     #answer.address = "bar"
     
-    #answers.push(answer)
+    #answers_received.push(answer)
   rescue
     puts "Unable to parse response"
     return nil
   end
-  return answers
+  return answers_received
 end
 
 ##################################################################
